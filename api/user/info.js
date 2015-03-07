972033d14230c7923ccea26518cf8842681e008c
Martin Hammerchmidt <martin.hammerchmidt@gmail.com>
2015-03-07 01:15:13 +0100

    add email in /user/info

--- api/user/info.js
+++ api/user/info.js
@@ -6,48 +6,49 @@ var resolver = require('../../resolver.js')
 /* Router function */
 function getInfos(req, res)
 {
-  var credentials = {};
-  credentials.accountKey = req.get('accountKey');
-  credentials.privateKey = req.get('privateKey');
+    var credentials = {};
+    credentials.accountKey = req.get('accountKey');
+    credentials.privateKey = req.get('privateKey');
 
-  auth.getUserFromAuth(credentials, function (err, user)
-  {
-    if (err != null)
+    auth.getUserFromAuth(credentials, function (err, user)
     {
-      tools.respondWithError('An error occurred. Check your credentials', res);
-      return;
-    }
+        if (err != null)
+        {
+            tools.respondWithError('An error occurred. Check your credentials', res);
+            return;
+        }
 
-    if (user === null)
-    {
-      tools.respondWithError('Check your credentials', res);
-      return;
-    }
+        if (user === null)
+        {
+            tools.respondWithError('Check your credentials', res);
+            return;
+        }
 
-    var db = resolver.resolve('db');
-    var users = db.collection('users');
+        var db = resolver.resolve('db');
+        var users = db.collection('users');
 
-    users.findOne(
-    {
-      _id: user
-    }, function (err, doc)
-    {
+        users.findOne(
+        {
+            _id: user
+        }, function (err, doc)
+        {
 
-      if (err != null)
-      {
-        tools.respondWithError('An error occurred', res);
-        return;
-      }
+            if (err != null)
+            {
+                tools.respondWithError('An error occurred', res);
+                return;
+            }
 
-      var response = {};
-      response.username = doc.username;
-      response.accountType = doc.accountType;
-      response.nOfFilesSaved = doc.nOfFilesSaved;
-      response.nOfViews = doc.nOfViews;
+            var response = {};
+            response.username = doc.username;
+            response.email = doc.email;
+            response.accountType = doc.accountType;
+            response.nOfFilesSaved = doc.nOfFilesSaved;
+            response.nOfViews = doc.nOfViews;
 
-      res.json(response);
+            res.json(response);
+        });
     });
-  });
 
 }
 
