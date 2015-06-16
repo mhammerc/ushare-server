2cce53847ff8d47b5261182389a438a7bb7f96fd
Martin Hammerchmidt <martin.hammerchmidt@gmail.com>
2015-06-16 02:58:46 +0200

    fix bug

--- beta-website/server/newsletter.js
+++ beta-website/server/newsletter.js
@@ -12,19 +12,23 @@ Meteor.methods(
 	{
 		check(email, String);
 
+ 		var self = this;
+		headers.get(self);
+		headers.ready(self);
+
 		if(!validator.isEmail(email))
 		{
 			throw new Meteor.Error('not-email', 'You\'re email isn\'t one.');
 			return;
 		}
 
-		if(hasAlreadyRegistered(email, headers.getClientIp()))
+		if(hasAlreadyRegistered(email, headers.get(self, 'x-real-ip')))
 		{
 			throw new Meteor.Error('already-registered', 'You are already registered.');
 			return;
 		}
 
-		Newsletter.insert({email:email, ip: headers.getClientIp()});
+		Newsletter.insert({email:email, ip: headers.get(self, 'x-real-ip')});
 
 		Mails.insert(
 		{
@@ -35,7 +39,7 @@ Meteor.methods(
 			sended: false
 	       });
 
-		console.log('Newsletter for ' + email + ' (' headers.getClientIp() + ')');
+		console.log('Newsletter for ' + email + ' (' +  headers.get(self, 'x-real-ip') + ')');
 	},
 	hasAlreadyRegistered: function(email) { return hasAlreadyRegistered(email, this.connection.clientAddress); }
 })
